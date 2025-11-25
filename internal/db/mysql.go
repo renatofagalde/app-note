b85b5261e93cc6460afe76691e84c685c4b55a69
Renato Fagalde <renato@likwi.com.br>
2025-11-24 21:25:27 -0300

    :sparkles: feat: Implement MySQL database connection using GORM

---
+++ internal/db/mysql.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Reaproveita o mesmo gormDatabase
+
+func NewMySQL(cfg Config) (Database, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?parseTime=true",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+
+	if err := sqlDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &gormDatabase{db: db}, nil
+}
