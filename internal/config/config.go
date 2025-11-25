b6cf8bc4849cad6277112f958508edb153a9fdc6
Renato Fagalde <renato@likwi.com.br>
2025-11-24 21:15:16 -0300

    :sparkles: feat: Refactor configuration loader by removing EnvLoader implementation

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,5 @@
 package config
 
-import (
-	"os"
-	"strconv"
-)
-
 type AppConfig struct {
 	AppPort          string
 	PostgresHost     string
@@ -17,37 +12,4 @@ type AppConfig struct {
 
 type Loader interface {
 	Load() (*AppConfig, error)
-}
-
-type EnvLoader struct{}
-
-func NewEnvLoader() Loader {
-	return &EnvLoader{}
-}
-
-func (EnvLoader) Load() (*AppConfig, error) {
-	return &AppConfig{
-		AppPort:          getenv("APP_PORT", "8080"),
-		PostgresHost:     getenv("POSTGRES_HOST", "localhost"),
-		PostgresPort:     mustInt(getenv("POSTGRES_PORT", "5433")),
-		PostgresUser:     getenv("POSTGRES_USER", "notes_user"),
-		PostgresPassword: getenv("POSTGRES_PASSWORD", "notes_password"),
-		PostgresDB:       getenv("POSTGRES_DB", "notes-db"),
-		PostgresSSL:      getenv("POSTGRES_SSLMODE", "disable"),
-	}, nil
-}
-
-func getenv(key, def string) string {
-	if v := os.Getenv(key); v != "" {
-		return v
-	}
-	return def
-}
-
-func mustInt(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err)
-	}
-	return i
 }
