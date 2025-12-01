f0d7e018b9391e207c93ea3d8106ba13161c37d4
Renato Fagalde <renato@likwi.com.br>
2025-11-30 22:03:16 -0300

    :bricks: ci: CORSMiddleware

---
+++ internal/http/CORSMiddleware.go
@@ -0,0 +1,22 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
